eb878c17b4252ac2dba93fbe0259a1f2cb38b8ab
erincodes <erinleeannscott@gmail.com>
2018-01-27 18:02:34 -0700

    Added html for instructions section, deleted revamp.html and instead put information in a .php file.

---
+++ revamped.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="css/revamped-style.css" type="text/css" />
+		<title>CSS Challenge</title>
+	</head>
+
+	<!--Sticky footer html template-->
+	<body class="Site">
+		<header>
+			<div class="container">
+				<h1>Dylan McDonald~$</h1>
+				<em>Computer Scientist. Cat Lover. Picard Fan.</em>
+			</div>
+		</header>
+		<main class="Site-content">
+			<img src="images/welcome.gif" alt="welcome to my homepage">
+			<div class="instructions-wrapper">
+				<div class="instructions-content">
+					<h2>Instructions</h2>
+					<ol>
+						<li>Fork and clone <a href="https://github.com/rlewis2892/css-challenge" target="_blank">this GitHub repository</a>, and create a new project and deployment.<br>
+						</li>
+						<li>Edit the stylesheet, adding CSS to make the page look like the mockup. <a href="images/mockup.png" target="_blank">Mockup Image</a></li>
+						<li>Have fun! The solution will be demonstrated in class. <small>(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧</small></li>
+						</ol>
+				</div>
+			</div>
+		</main>
+		<section>
+		</section>
+		<footer>
+		</footer>
+	</body>
+</html>
+
